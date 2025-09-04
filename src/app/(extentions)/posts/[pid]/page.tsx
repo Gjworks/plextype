743a93ef5075cda2183fc07ecfb2526f2d92b02b
Gjworks <gjworks@kakao.com>
2025-09-04 18:32:20 +0900

    게시판 카테고리 할 수 있도록 추가

--- src/app/(extentions)/posts/[pid]/page.tsx
+++ src/app/(extentions)/posts/[pid]/page.tsx
@@ -4,22 +4,20 @@ import PostsList from "@/extentions/posts/templates/default/listAction";
 // type Params = Promise<{ pid: string }>;
 // type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-type Params = { pid: string };
-type SearchParams = { [key: string]: string | string[] | undefined };
+type Params = Promise<{ pid: string }>;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 const Page = async (props: { params: Params; searchParams: SearchParams }) => {
-  const params = props.params;
-  const searchParams = props.searchParams;
+  const params = await props.params;
+  const searchParams = await props.searchParams;
   const query = searchParams.query;
 
   return (
-    <>
-      <div className={`max-w-screen-xl mx-auto px-3`}>
+      <div className="max-w-screen-xl mx-auto px-3">
         <Suspense fallback={<div>Loading posts...</div>}>
           <PostsList params={{ pid: params.pid }} />
         </Suspense>
       </div>
-    </>
   );
 };
 
