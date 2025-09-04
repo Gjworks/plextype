cb0fd485073f9e4e24023c5cdca1f6c522018e20
Gjworks <gjworks@kakao.com>
2025-09-04 18:36:35 +0900

    Promise 오류

--- src/app/(extentions)/posts/[pid]/view/[id]/page.tsx
+++ src/app/(extentions)/posts/[pid]/view/[id]/page.tsx
@@ -2,13 +2,13 @@ import PostsRead from "@/extentions/posts/templates/default/readAction";
 
 type Params = { pid: string; id: string };
 
-const Page = async (props: { params: Params }) => {
-  const params = props.params;
+const Page = async (props: { params: Promise<Params> }) => {
+  const params = await props.params;
   console.log(params.id);
+
   return (
-    <>
       <PostsRead params={{ pid: params.pid, id: params.id }} />
-    </>
   );
 };
+
 export default Page;
