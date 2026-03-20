5f27ec9b6ef1005e727e7b71796362460a35ad9c
Gjworks <gjworks@kakao.com>
2026-03-20 14:38:45 +0900

    추가 파일들

---
+++ src/app/(extentions)/admin/user/create/page.tsx
@@ -0,0 +1,15 @@
+import UpsertForm from "@extentions/user/_admin/upsertForm";
+import { getAllGroupRecords } from "@extentions/user/_actions/group.action";
+
+const Page = async () => {
+  const groupList = await getAllGroupRecords();
+
+  return (
+    <div className="max-w-screen-2xl mx-auto px-3">
+      {/* 💡 user 데이터를 안 넘깁니다! -> 자동으로 '등록 모드' 발동 */}
+      <UpsertForm groupList={groupList} />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(extentions)/admin/user/groupList/page.tsx
@@ -0,0 +1,18 @@
+// src/app/(extentions)/admin/user/groupList/page.tsx
+import DashboardUserGroupList from "@extentions/user/_admin/groupList";
+// 💡 경로를 group.action 으로 명확하게 지정해 줍니다!
+import { getAllGroupRecords } from "@extentions/user/_actions/group.action";
+
+const Page = async () => {
+  // 💡 1. 서버가 화면을 그리기 전에 DB를 직통으로 찔러서 그룹 목록을 싹 가져옵니다.
+  const groupList = await getAllGroupRecords();
+
+  return (
+    <div className="max-w-screen-2xl mx-auto py-10">
+      {/* 💡 2. 클라이언트 컴포넌트에는 '완성된 데이터'만 쏙 던져줍니다. */}
+      <DashboardUserGroupList initialGroupList={groupList} />
+    </div>
+  );
+};
+
+export default Page;
