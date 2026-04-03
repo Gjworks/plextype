507f6ce632c2ac7974ba0832177cb478fc8b7874
Gjworks <gjworks@kakao.com>
2026-04-03 14:20:32 +0900

    메인 컨텐츠 위젯 게시판 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Page() {
                       className="relative mx-auto flex items-center gap-4"
                     >
                       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[380px] h-[80px] bg-[url(/assets/svg/dotted.svg)] bg-cover bg-center bg-no-repeat"></div>
-                      <div className="dark:bg-dark-800 rounded-lg bg-white/5 border border-white/10 backdrop-blur-sm p-3">
+                      <div className="text-white dark:bg-dark-800 rounded-lg bg-gray-950/5 border border-white/10 backdrop-blur-sm p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
                         </svg>
