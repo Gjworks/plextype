e3dd8d1355c749c4f556e157257e17a6ecc4aebe
Gjworks <gjworks@kakao.com>
2026-04-17 13:50:22 +0900

    fix: meta태그 변경 및 document의 thumbnail 컬럼추가

--- src/app/(modules)/posts/[mid]/[slug]/page.tsx
+++ src/app/(modules)/posts/[mid]/[slug]/page.tsx
@@ -2,30 +2,21 @@
 import React, { Suspense } from "react";
 import { redirect } from "next/navigation";
 import { Post } from "@/modules/posts"; // 🌟 스마트 블록
-import {getDocumentBySlugAction} from "@/modules/document/actions/document.action";
-import { getSeoMetadata } from "@utils/helper/matadata";
+import { getPostReadMetadata } from "@/modules/posts/actions/seo.action";
 
 interface PageProps {
   params: Promise<{ mid: string; slug: string }>;
   searchParams?: Promise<{ page?: string }>;
 }
 
-export async function generateMetadata({ params }: { params: Promise<{ mid: string; slug: string }> }) {
-  const { mid, slug } = await params;
-  if (slug === "create") return getSeoMetadata({ title: "글 작성" });
-
-  const res = await getDocumentBySlugAction(slug);
-  const doc = res.data;
-
-  return getSeoMetadata({
-    title: `${process.env.PROJECT_TITLE} - ${doc?.title ?? "게시글"}`,
-    description: `${doc?.user?.nickName ?? "작성자"}님의 글입니다.`,
-  });
+export async function generateMetadata({ params }: any) {
+  const { slug, mid } = await params;
+  return await getPostReadMetadata(slug, mid); // 👈 한 줄 컷!
 }
 
 const Page = async ({ params, searchParams }: PageProps) => {
   const resolvedParams = await params;
-  console.log("🚀 지금 들어온 파라미터:", resolvedParams);
+
   const { mid, slug } = await params;
   const { page } = (await searchParams) || {};
   const docId = slug;
