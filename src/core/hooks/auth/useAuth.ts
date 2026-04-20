84209141bad88b575d3bfe21621b4070b6823b69
Gjworks <gjworks@kakao.com>
2026-04-20 11:06:10 +0900

    app 디렉토리 gitignore처리

--- src/core/hooks/auth/useAuth.ts
+++ src/core/hooks/auth/useAuth.ts
@@ -21,9 +21,11 @@ export const useUser = (options?: Partial<UseQueryOptions<any, Error>>) => {
     queryKey: ["user"],
     queryFn: fetchUser,
     retry: false, // 로그인 실패 시 재시도 안 함
-    staleTime: 5 * 60 * 1000, // 5분 동안 데이터가 신선하다고 간주 (새로고침 시에도 기존 데이터 사용)
+    staleTime: 15 * 60 * 1000, // 5분 동안 데이터가 신선하다고 간주 (새로고침 시에도 기존 데이터 사용)
+    refetchInterval: 45 * 60 * 1000,
+    refetchIntervalInBackground: true,
     // cacheTime: 10 * 60 * 1000, // 캐시된 데이터가 10분 동안 유지됨
-    refetchOnWindowFocus: false, // 윈도우가 다시 포커스될 때 자동으로 데이터를 새로 고침하지 않도록 설정
+    refetchOnWindowFocus: true, // 윈도우가 다시 포커스될 때 자동으로 데이터를 새로 고침하지 않도록 설정
     ...options,
   };
 
