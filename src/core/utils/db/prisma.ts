b264778449e319dde6c8517fa8f7c0086d677735
Gjworks <gjworks@kakao.com>
2026-04-28 18:27:33 +0900

    fix: Document, Comment, Attachment, User에 status 컬럼 추가

--- src/core/utils/db/prisma.ts
+++ src/core/utils/db/prisma.ts
@@ -1,10 +1,27 @@
-import { PrismaClient, PermissionSubject, Prisma } from "@prisma/client"; // Prisma 추가
+import { PrismaPg } from "@prisma/adapter-pg";
+import {
+  PrismaClient,
+  PermissionSubject,
+  Prisma,
+} from "@/core/utils/db/generated/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
 export { PermissionSubject, Prisma };
