2a103a350c6fd76eb4b7dc8c8aafa183b53f65fd
Gjworks <gjworks@kakao.com>
2026-04-29 11:22:23 +0900

    prisma client 경로 수정

--- src/modules/posts/actions/_type.ts
+++ src/modules/posts/actions/_type.ts
@@ -1,4 +1,4 @@
-import { Prisma, PermissionSubject } from "@prisma/client";
+import { Prisma, PermissionSubject } from "@core/utils/db/generated/client";
 import { ActionResponse } from "@/core/types/actions";
 import { z } from "zod";
 
